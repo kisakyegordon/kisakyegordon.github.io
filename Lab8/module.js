e3e9fb3d9473ccdfad94eaf66779ed7034a22ee1
kisakyegordon <gordon56692@gmail.com>
2020-06-01 18:19:44 -0500

    final changes

--- Lab8/module.js
+++ Lab8/module.js
@@ -1,33 +1,43 @@
-var Account = function(){
+(function() {
+    "use script"
+
+    const accountCreator = (function() {
+        let accountName;
+        let amount;
+
+        return {
+            createAccount: function (name, amt) {
+                accountName = name;
+                amount = amt;
+
+                return {name: accountName, balance: amount};
+            }
+        }
+    })();
+document.getElementById("create-button").onclick = handleAddition;
+
+
+    var accountList = [];
+
+    function handleAddition() {
+        const acc_name = document.getElementById("account-name").value;
+        const acc_deposit = document.getElementById("account-deposit").value;
+        const acc_display = document.getElementById("view-accounts").value;
     
-    var myAccounts = [];
     
-
-    return {
-        addAccount: function addAccount(account) {
-            myAccounts.push(account);
-        },
-        myAccounts: myAccounts
-    }    
-
-}
-
-var mine = Account;
-
-const acc_name = document.getElementById("account-name").value;
-const acc_deposit = document.getElementById("account-deposit").value;
-const acc_button = document.getElementById("create-button");
-const acc_display = document.getElementById("view-accounts").value;
-
-function handleAddition({acc_name, acc_deposit}) {
-    console.log("Here we go")
-    mine.addAccount({acc_name, acc_deposit})
-    acc_display = "";
-
-    for(const element of mine.myAccounts) {
-        acc_display = element + "\n"
+        const newAccount = accountCreator.createAccount(acc_name, acc_deposit);
+        accountList.push(newAccount);
+        document.getElementById("view-accounts").value = "";
+    
+        for(const element of accountList) {
+            document.getElementById("view-accounts").value += `Account Name: ${element.name}, Balance: ${element.balance} \n `;
+        }
+    
     }
+})()
 
-}
 
-acc_button.addEventListener("onclick", handleAddition({acc_name, acc_deposit}));
+
+
+
+
